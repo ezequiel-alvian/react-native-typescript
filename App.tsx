ff3f57a32209f92495010beb03df9c4abd638b7b
ezalvian <ezequiel.alvian@swissmedical.com.ar>
2023-06-03 10:00:55 -0300

    first-change

--- App.tsx
+++ App.tsx
@@ -1,13 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
+import { Ionicons } from '@expo/vector-icons'
+
+import HomeScreen from './components/HomeScreen'
+import SettingsScreen from './components/SettingsScreen'
+import ProductsScreen from './components/ProductsScreen'
+
+const Tab = createBottomTabNavigator();
+
+type TabIcon = {
+  focused: boolean
+  color: string
+  size: number
+}
+type RouteOp = {
+  route:{
+    name:string
+  }
+}
+
+
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }:RouteOp) => ({
+          tabBarIcon: ({ focused, color, size }:TabIcon) => {
+            let hola!: string 
+            if (route?.name === 'Home') {
+              hola = focused
+                ? 'home'
+                : 'home';
+            } else if (route?.name === 'Settings') {
+              hola = focused 
+                ? 'md-cart-outline' 
+                : 'md-cart-outline'
+            }
+            else if (route?.name === 'ProductsScreen') {
+              hola = focused 
+                ? 'list-circle-outline' 
+                : 'list'
+            }
+            return <Ionicons 
+                      name={hola} 
+                      size={size} 
+                      color={color} 
+                    />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="ProductsScreen" component={ProductsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +70,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
---
+++ components/ProductsScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const ProductsScreeen = () => {
+    return(
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Products</Text>
+    </View>
+    )
+}
+
+export default ProductsScreeen
