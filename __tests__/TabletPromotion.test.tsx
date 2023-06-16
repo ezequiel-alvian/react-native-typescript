c3dbeee1306756c0814046439dd74a3e3d07281f
ezalvian <ezequiel.alvian@swissmedical.com.ar>
2023-06-16 13:16:43 -0300

    changes snapshot tabltpromotion

--- __tests__/TabletPromotion.test.tsx
+++ __tests__/TabletPromotion.test.tsx
@@ -7,29 +7,6 @@ describe('<TabletPromotion/>',()=>{
     let data = 25
     test('test',()=>{
         component = render(<TabletPromotion promotion={data}/>)
-        expect(component.toJSON()).toMatchInlineSnapshot(`
-<View
-  style={
-    {
-      "alignItems": "flex-end",
-      "display": "flex",
-      "flexDirection": "column",
-      "justifyContent": "flex-end",
-      "width": 710,
-    }
-  }
->
-  <Text
-    style={
-      {
-        "backgroundColor": "#00FF7F",
-        "padding": 5,
-      }
-    }
-  >
-    -25%
-  </Text>
-</View>
-`)
-    })
+        expect(component.toJSON()).toMatchSnapshot()
+      })
 })
